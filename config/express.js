e502a50e9c33edfb2be7cbece7e7e1ac5074dbfd
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2016-02-17 20:55:07 -0800

    Morgan api update.

--- config/express.js
+++ config/express.js
@@ -62,7 +62,7 @@ module.exports = function (app, passport) {
 
   // Don't log during tests
   // Logging middleware
-  if (env !== 'test') app.use(morgan(log));
+  if (env !== 'test') app.use(morgan('default',log));
 
   // Swig templating engine settings
   if (env === 'development' || env === 'test') {
