bf9ac9457080e1df191192fa628b5a8f10f0694d
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2016-02-29 18:30:40 -0800

    Cleaning up messages.

--- app/js/components/PageMain.react.js
+++ app/js/components/PageMain.react.js
@@ -73,97 +73,39 @@ const ArticleSection = React.createClass({
 
     var mainPage
     if ( page ) {
-      // const favicon = page.faviconCDN?<img onError={this._imgError} src={page.faviconCDN} className="favicon" />:null;
-      // const text = _.map(page.text, function(val, i){
-      //     return <Para 
-      //       key={i} 
-      //       _key={i}
-      //       onDown={that._onDown}
-      //       onUp={that._onUp}
-      //       text={val}
-      //       tags={_.filter(page.links,{paragraphIndex:i})} />
-      // });
-
-      // const url = parse(page.canonicalLink, true);
-      // const prettyLink = url.host+url.pathname;
-
-      // var image;
-      // if ( page.imageCDN.url ){ // Create the image and set the height from the DB before we load it.
-      //   const imgHeight = 700<page.imageCDN.dimensions[0]?
-      //     700/page.imageCDN.dimensions[0]*page.imageCDN.dimensions[1]:
-      //     page.imageCDN.dimensions[1];
-
-      //   image = 
-      //     <div className="page-img" style={{height:imgHeight}} >
-      //       <img onError={this._imgError} src={page.imageCDN.url} style={{maxWidth:'100%'}} />
-      //     </div>
-      // } else { // We dont have an image.
-      //   image = 
-      //     null
-      // }
-
-      // const location = this.state.selectionLocation;
-      // const toolTip = location ?
-      //   <ToolTip 
-      //     location={location} 
-      //     onClick={this._onToolTipClick} />:
-      //   null;
-
-      mainPage =  <PageArticle onSelectionClick={this._onClick} page={page} />;
-
-      // const m = <div className="row">
-      //   <div className="page-main">
-      //     <div className="header">
-      //       <h1>{page.title}</h1>
-      //       <div className="page-link">
-      //         {favicon}
-      //         &nbsp;
-      //         <a href={page.canonicalLink} target="_blank">{prettyLink}</a>
-      //       </div>
-      //       <p style={{color:"#999", marginBottom:"24px"}}>{page.description}</p>
-      //       {image}
-      //     </div>
-      //     <div className="page-text">
-      //       <ReactCSSTransitionGroup transitionAppear={true} transitionName="fall" transitionAppearTimeout={200} transitionEnterTimeout={200} transitionLeaveTimeout={1} >
-      //         {toolTip}
-      //       </ReactCSSTransitionGroup>
-      //       {text}
-      //     </div>
-      //   </div>
-      // </div>
-
-      } else {
-        mainPage = <PageSearch />
-      }
+    mainPage =  <PageArticle onSelectionClick={this._onClick} page={page} />;
+    } else {
+      mainPage = <PageSearch />
+    }
       
-      const pageConnect = linkedPage?<PageConnect page={linkedPage} />:null;
-      return <div>
-        <section className="container ease">
-          {errorMessage}
-          <div className="content main">
-            <button onClick={this._createLink} type="button" className="btn btn-default">
-            </button>
-            <ReactCSSTransitionGroup transitionAppear={true} transitionName="fall" transitionAppearTimeout={500} transitionEnterTimeout={500} transitionLeaveTimeout={500} >
-              {pageConnect}
-            </ReactCSSTransitionGroup>
-            <div className="row connect-action" style={{display:'none'}}>
-              <a style={{width:'300px', margin: 'auto', fontSize:'2rem'}} className="btn btn-default" > Create Link </a>
-            </div>
-            {mainPage}
+    const pageConnect = linkedPage?<PageConnect page={linkedPage} />:null;
+    return <div>
+      <section className="container ease">
+        {errorMessage}
+        <div className="content main">
+          <button onClick={this._createLink} type="button" className="btn btn-default">
+          </button>
+          <ReactCSSTransitionGroup transitionAppear={true} transitionName="fall" transitionAppearTimeout={500} transitionEnterTimeout={500} transitionLeaveTimeout={500} >
+            {pageConnect}
+          </ReactCSSTransitionGroup>
+          <div className="row connect-action" style={{display:'none'}}>
+            <a style={{width:'300px', margin: 'auto', fontSize:'2rem'}} className="btn btn-default" > Create Link </a>
           </div>
-        </section>
-      </div>
+          {mainPage}
+        </div>
+      </section>
+    </div>
   },
   /**
    * Event handler for 'change' events coming from the PageStore
    */
   _onClick: function(data) {
     console.log(data)
-        // that.setState({
-        //   selectedParagraphIndex: paragraphIndex,
-        //   selectedIndex: [start, end],
-        //   selectionLocation: getSelectionCoords() // wee use the timeout to make sure the dom has time register the selection 
-        // })
+    that.setState({
+      selectedParagraphIndex: paragraphIndex,
+      selectedIndex: [start, end],
+      selectionLocation: getSelectionCoords() // wee use the timeout to make sure the dom has time register the selection 
+    })
   },
   /**
    * Event handler for 'change' events coming from the PageStore
